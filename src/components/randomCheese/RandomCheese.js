96d7529161363f4c0fe606250b03cc5971bd4b7f
charlottemothersole <charlottemothersole@gmail.com>
2023-08-17 16:12:36 +0100

    set up component and removed strict mode on index

--- src/components/randomCheese/RandomCheese.js
+++ src/components/randomCheese/RandomCheese.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState } from 'react';
 const RandomCheese = () => {
+    const [cheese, setCheese] = useState([]);
+    
+    useEffect(() => {
+        fetch("/api/cheeses/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setCheese(data);
+        } )
+    }, [])
     return ( 
-        <h1>Random Cheese</h1>
+        <div>
+            <h1>Random Cheese</h1>
+            <h2>{ cheese.name }</h2>
+        </div>
+        
      );
 }
- 
+    
 export default RandomCheese;
