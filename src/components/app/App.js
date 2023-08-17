11dfe1e80bc5130095f4102b067bd77dcfe62977
Sam <sam.ryan101@gmail.com>
2023-08-17 19:48:00 +0100

    this method is way easier, fixed and updated

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,29 @@
-import './App.css';
-import './App.css';
-import RandomCheese from '../randomCheese/RandomCheese'
-import LoginForm from '../auth/LoginForm'
-import SignUpForm from '../user/SignUpForm'
+import "./App.css";
+import RandomCheese from "../randomCheese/RandomCheese";
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../user/SignUpForm";
 
-import {
-  useNavigate,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useNavigate, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-
-      <Routes>
-          <Route path='/'  element={<RandomCheese navigate={ useNavigate() }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-        </Routes>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route
+                    path="/"
+                    element={<RandomCheese navigate={useNavigate()} />}
+                />
+                <Route
+                    path="/login"
+                    element={<LoginForm navigate={useNavigate()} />}
+                />
+                <Route
+                    path="/signup"
+                    element={<SignUpForm navigate={useNavigate()} />}
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
