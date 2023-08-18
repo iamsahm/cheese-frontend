78e04136d8cb29695860757c60d22af55985d2f3
charlottemothersole <charlottemothersole@gmail.com>
2023-08-18 17:49:28 +0100

    use cheese cleaner and add more detail

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -21,7 +21,26 @@ const themeOptions = createTheme({
       main: '#f50057',
       },
   },
+  typography: {
+    h1: {
+      fontSize: '50px',
+    },
+    h2: {
+      fontSize: '18px',
+      fontWeight: '600',
+    },
+    randomCheeseFields: {
+      fontSize: '18px',
+      fontWeight: '400'
+    }
+  }
 });
+
+themeOptions.typography.h1 = {
+  fontSize: '50px',
+  // fontWeight: 'bold',
+};
+
 
 // const theme = {
 //   fg: '#FFAB00',
--- src/components/randomCheese/RandomCheese.js
+++ src/components/randomCheese/RandomCheese.js
@@ -1,18 +1,10 @@
 import {useState, useEffect} from 'react';
-import Avatar from '@mui/material/Avatar';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom'; 
 import LogInForm from './/../auth/LoginForm';
-import StyledButton from '../app/styledButton';
 
 
 const RandomCheese = () => {
@@ -21,25 +13,15 @@ const RandomCheese = () => {
     
     useEffect(() => {
         fetch("/api/cheeses/random")
-        .then((response) => response.json())
-        .then((data) => {
-            setCheese(data);
-        } )
+        .then((response) => {
+            response.json()
+            .then((data) => {
+                setCheese(data.data);
+            } )
+        })
+        
     }, [])
-
     const link = cheese.image;
-
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const data = new FormData(event.currentTarget);
-    //     console.log({
-    //         email: data.get('email'),
-    //         password: data.get('password'),
-    //     });
-    //     // You can add further logic here, such as sending the form data to the server
-    // };
-
-
     return (
         <Grid container component="main" sx={{ height: '100vh' }}>
             {/* Left side: Login box */}
@@ -56,8 +38,40 @@ const RandomCheese = () => {
                         alignItems: 'lower',
                         justifyContent: 'center'}}
                 >    
-                <img src={link} alt={cheese.name} style={{ width: '100%', maxWidth: '600px' }} />
-                <Typography variant="h4">{cheese.name}</Typography>
+                    <Typography variant="h1">Random Cheese</Typography>
+                    <img src={link} alt={cheese.name} style={{ width: '100%', maxWidth: '600px' }} />
+                    <Typography variant="h4">{cheese.name}</Typography>
+                    <Typography variant="h2">Cheese Type: </Typography>
+                    {/* this conditional checks if exists once, then if not does nothing, then is rechecked when useEffect is called/component is re-rendered*/}
+                    <Typography variant="p" className='randomCheeseFields'>{cheese?.type?.map((item) => (
+                        <p key={item}>{item}</p>
+                    ))}
+                    </Typography>
+                    <Typography variant="h2">Description: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese.description}</Typography>
+                    <Typography variant="h2">Flavour Profiles: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese.flavour}</Typography>
+                    <Typography variant="h2">Cheese Families: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese.family}</Typography>
+                    <Typography variant="h2">Aromas: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese?.aromas?.map((item) => (
+                        <p key={item}>{item}</p>
+                    ))}
+                    </Typography>
+                    <Typography variant="h2">Regions of Origin: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese.region}</Typography>
+                    <Typography variant="h2">Countries of Origin: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese?.countries?.map((item) => (
+                        <p key={item}>{item}</p>
+                    ))}
+                    </Typography>
+                    <Typography variant="h2">Milk Type: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{cheese?.milks?.map((item) => (
+                        <p key={item}>{item}</p>
+                    ))}
+                    </Typography>
+                    <Typography variant="h2">Suitable for Vegetarians: </Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{ cheese.vegetarian ? cheese.vegetarian : 'Unknown'}</Typography>
                 </Box>
             </Grid>
         </Grid>
