ea6e3b6878dbdb7bcd8c1767bbd0534641b69b07
charlottemothersole <charlottemothersole@gmail.com>
2023-08-21 11:44:38 +0100

    add more styling

--- src/components/randomCheese/RandomCheese.js
+++ src/components/randomCheese/RandomCheese.js
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom'; 
 import LogInForm from './/../auth/LoginForm';
 
-
 const RandomCheese = () => {
     const [cheese, setCheese] = useState([]);
     const navigate = useNavigate(); 
@@ -22,6 +21,7 @@ const RandomCheese = () => {
         
     }, [])
     const link = cheese.image;
+
     return (
         <Grid container component="main" sx={{ height: '100vh' }}>
             {/* Left side: Login box */}
@@ -30,7 +30,9 @@ const RandomCheese = () => {
             </Grid>    
         
             {/* Right side: Random Cheese image */}
-            <Grid item xs={false} sm={10} md={4}>
+            <Grid item xs={false} sm={10} md={4} sx={{
+                marginLeft: '30px',
+            }}>
                 <Box
                     sx={{
                         display: 'flex',
@@ -39,36 +41,57 @@ const RandomCheese = () => {
                         justifyContent: 'center'}}
                 >    
                     <Typography variant="h1">Random Cheese</Typography>
-                    <img src={link} alt={cheese.name} style={{ width: '100%', maxWidth: '600px' }} />
+                    <img src={link} alt={cheese.name} style={{ width: '100%', maxWidth: '350px', height: '250px', margin: '15 0' }} />
                     <Typography variant="h4">{cheese.name}</Typography>
                     <Typography variant="h2">Cheese Type: </Typography>
                     {/* this conditional checks if exists once, then if not does nothing, then is rechecked when useEffect is called/component is re-rendered*/}
-                    <Typography variant="p" className='randomCheeseFields'>{cheese?.type?.map((item) => (
-                        <p key={item}>{item}</p>
+                    <Typography variant="p" className='randomCheeseFields' style={{ display: 'flex', flexDirection: 'row' }}>{cheese?.type?.map((item) => (
+                        <p key={item} style={{ margin: '5px 10px 5px 0' }}>{item} </p>
                     ))}
                     </Typography>
                     <Typography variant="h2">Description: </Typography>
                     <Typography variant="p" className='randomCheeseFields'>{cheese.description}</Typography>
                     <Typography variant="h2">Flavour Profiles: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese.flavour}</Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{ cheese.flavour ? cheese.flavour : 'Unknown'}</Typography>
                     <Typography variant="h2">Cheese Families: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese.family}</Typography>
-                    <Typography variant="h2">Aromas: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese?.aromas?.map((item) => (
-                        <p key={item}>{item}</p>
-                    ))}
+                    <Typography variant="p" className='randomCheeseFields'>{ cheese.family ? cheese.family : 'Unknown'}</Typography>
+                    <Typography variant="h2">countries: </Typography>
+                    <Typography 
+                    variant="p" 
+                    className='randomCheeseFields' 
+                    style={{ 
+                        display: 'flex', 
+                        flexDirection: 'row' 
+                    }}
+                    >{cheese.aromas && cheese.aromas.length > 0 ? cheese?.aromas?.map((item) => (
+                        <p key={item} style={{ margin: '5px 10px 5px 0' }}>{ item }</p>
+                    )) : <p style={{ margin: '5px 10px 5px 0' }}>Unknown</p>}
                     </Typography>
                     <Typography variant="h2">Regions of Origin: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese.region}</Typography>
+                    <Typography variant="p" className='randomCheeseFields'>{ cheese.region ? cheese.region : 'Unknown'}</Typography>
                     <Typography variant="h2">Countries of Origin: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese?.countries?.map((item) => (
-                        <p key={item}>{item}</p>
-                    ))}
+                    <Typography 
+                    variant="p" 
+                    className='randomCheeseFields' 
+                    style={{ 
+                        display: 'flex', 
+                        flexDirection: 'row' 
+                    }}
+                    >{cheese.countries && cheese.countries.length > 0 ? cheese?.countries?.map((item) => (
+                        <p key={item} style={{ margin: '5px 10px 5px 0' }}>{ item }</p>
+                    )) : <p style={{ margin: '5px 10px 5px 0' }}>Unknown</p>}
                     </Typography>
                     <Typography variant="h2">Milk Type: </Typography>
-                    <Typography variant="p" className='randomCheeseFields'>{cheese?.milks?.map((item) => (
-                        <p key={item}>{item}</p>
-                    ))}
+                    <Typography 
+                    variant="p" 
+                    className='randomCheeseFields' 
+                    style={{ 
+                        display: 'flex', 
+                        flexDirection: 'row' 
+                    }}
+                    >{cheese.milks && cheese.milks.length > 0 ? cheese?.milks?.map((item) => (
+                        <p key={item} style={{ margin: '5px 10px 5px 0' }}>{ item }</p>
+                    )) : <p style={{ margin: '5px 10px 5px 0' }}>Unknown</p>}
                     </Typography>
                     <Typography variant="h2">Suitable for Vegetarians: </Typography>
                     <Typography variant="p" className='randomCheeseFields'>{ cheese.vegetarian ? cheese.vegetarian : 'Unknown'}</Typography>
